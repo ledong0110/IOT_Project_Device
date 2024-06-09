d27eee5819cd31b35845f8914a82af4e55957319
ledong0110 <74060032+ledong0110@users.noreply.github.com>
2024-06-10 00:44:41 +0700

    fix: bug in config

--- src/tasks/read_sensors.py
+++ src/tasks/read_sensors.py
@@ -1,10 +1,10 @@
 from typing import List
-from ..device_wrapper import RS485
+from ..device_wrapper import Relay, Sensor
 from ..connector import AdafruitConnector
 import json
 from ..config import glob_var
 
 
 def read_sensors(feed_id: str):
-    data = json.dumps({sensor.get_name(): sensor.read() for sensor in glob_var.list_sensors+glob_var.list_actuators})
+    data = json.dumps({sensor.get_name(): sensor.read() if type(sensor) is Sensor else sensor.get_state() for sensor in glob_var.list_sensors+glob_var.list_actuators})
     glob_var.mqtt_client.publish(feed_id=feed_id, content=data)
