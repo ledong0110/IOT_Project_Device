659d48cf7746fa0f1079715a42b9c13fd11cdcad
ledong0110 <74060032+ledong0110@users.noreply.github.com>
2024-06-11 23:28:47 +0700

    update

--- src/config/glob_var.py
+++ src/config/glob_var.py
@@ -1,5 +1,5 @@
 from ..connector import AdafruitConnector
 
-mqtt_client = AdafruitConnector()
+mqtt_client = AdafruitConnector(feed_ids=["ledong0110/feeds/task-action", "ledong0110/feeds/task-result-query"])
 list_sensors = []
 list_actuators = []
